f30ff6cde695c2da4a6ffa9730a1dc7bebc4b812
JustinVegas <guardtown123@gmail.com>
2025-07-11 11:43:51 -0400

    [ADD]agregando seguridad a las rutas

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,13 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-const publicRoutes = ["/login"];
-const privateRoutes = [''];
+const publicRoutes = ["/login", "/sign-up"];
+const privateRoutes = [
+  "/dashboard",
+  "/Profile",
+  "/admin/*",
+  "Torneos/[id]/inscription",
+];
 
 export const config = {
   matcher: [...publicRoutes, ...privateRoutes],
@@ -21,17 +26,34 @@ export default withAuth(
     callbacks: {
       authorized: ({ token, req }) => {
         const { pathname } = req.nextUrl;
-        
+
         if (publicRoutes.includes(pathname)) {
           return true;
         }
-        if (privateRoutes.includes(pathname)) {
+        const isPrivateRoute = privateRoutes.some((route) => {
+          if (route.endsWith("/*")) {
+            const baseRoute = route.replace("/*", "");
+            return pathname.startsWith(baseRoute);
+          } else if (route.includes("[id]")) {
+            // Patrón para /Torneos/[id]/inscription
+            const routeParts = route.split("/");
+            const pathParts = pathname.split("/").splice(1);
+            if (routeParts.length !== pathParts.length) return false;
+            return routeParts.every((part, i) => {
+              return part.startsWith("[") || part === pathParts[i];
+            });
+          }
+          return pathname === route;
+        });
+
+        if (isPrivateRoute) {
           return !!token;
         }
         return true;
       },
     },
     pages: {
+      signIn: "/login",
     },
   }
 );
